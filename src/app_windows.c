6ddb9ba5c7d6bc0346a7741c4b8b1b6de59d172a
Ivan Avdeev <me@provod.works>
2024-10-22 15:55:54 -0400

    app: add wonitors-based display+edid enumeration

--- src/app_windows.c
+++ src/app_windows.c
@@ -21,6 +21,11 @@
 #include <atto/platform.h> */
 #include <atto/app.h>
 
+#include <ShellScalingApi.h> // GetDpiForMonitor() and friends
+#include <Setupapi.h> // SetupDiGetClassDevsEx()
+#include <initguid.h> // Must be before ntddvdeo.h to avoid linking errors (?! wtf)
+#include <ntddvdeo.h> // GUID_DEVINTERFACE_MONITOR
+
 /* static WCHAR *utf8_to_wchar(const char *string, int length, int *out_length); */
 static char *wchar_to_utf8(const WCHAR *string, int length, int *out_length);
 static void a__AppOpenConsole(void);
@@ -39,6 +44,8 @@ static const PIXELFORMATDESCRIPTOR a__pfd = {sizeof(a__pfd), 0,
 static LARGE_INTEGER a__time_freq;
 static LARGE_INTEGER a__time_start;
 
+#define A__MAX_DISPLAYS 16
+
 static struct {
 	HWND hwnd;
 	HDC hdc;
@@ -48,6 +55,9 @@ static struct {
 		int resetAbsolute;
 		long x, y;
 	} rawMouse;
+
+	int displays_count;
+	AAppDisplay displays[A__MAX_DISPLAYS];
 } g;
 
 ATimeUs aAppTime(void) {
@@ -75,35 +85,256 @@ void aAppTerminate(int code) {
 	ExitProcess(code);
 }
 
+static void printError(const char *msg, DWORD error) {
+	LPSTR pbuf = NULL;
+	const DWORD size = FormatMessageA(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, error,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&pbuf, 0, NULL
+	);
+	aAppDebugPrintf("ERROR: %s: code=%#lx(%lu): %.*s", msg, error, error, (int)size, pbuf);
+	LocalFree(pbuf);
+}
+
+#define printLastError(msg) printError(msg, GetLastError())
+
+static void printDisplayDevice(const char *prefix, const DISPLAY_DEVICE *dd) {
+	aAppDebugPrintf("%sDeviceName: %s", prefix, dd->DeviceName);
+	aAppDebugPrintf("%sDeviceString: %s", prefix, dd->DeviceString);
+	aAppDebugPrintf("%sStateFlags: %#lx", prefix, dd->StateFlags);
+	aAppDebugPrintf("%sDeviceID: %s", prefix, dd->DeviceID);
+	aAppDebugPrintf("%sDeviceKey: %s", prefix, dd->DeviceKey);
+}
+
+typedef struct {
+	BYTE data[128];
+} EDID;
+
+static BOOL readEdidForDisplayPath(const char *monitor_id, EDID *out_edid) {
+	BOOL result = FALSE;
+
+	// Start a query for all monitors device interfaces
+	const HDEVINFO devs =
+			SetupDiGetClassDevs(&GUID_DEVINTERFACE_MONITOR, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+	if (devs == INVALID_HANDLE_VALUE) {
+		printLastError("SetupDiGetClassDevs");
+		return FALSE;
+	}
+
+	// Iterate through all monitor device interfaces
+	for (DWORD iface_index = 0;; iface_index++) {
+		// Get device interface data. It's needed in order to get interface detail
+		SP_DEVICE_INTERFACE_DATA iface_data = {
+				.cbSize = sizeof(iface_data),
+		};
+		if (!SetupDiEnumDeviceInterfaces(devs, NULL, &GUID_DEVINTERFACE_MONITOR, iface_index, &iface_data))
+			break;
+		aAppDebugPrintf("    iface[%lu]:", iface_index);
+
+		// Get interface detail. It contains device path string, which is the same
+		// as DeviceID for DISPLAY_DEVICE, see EDD_GET_DEVICE_INTERFACE_NAME flag
+		// comment below.
+
+		// Interface detail is obtained in two steps:
+		// 1. First, read the needed size for the string
+		DWORD detail_size = 0;
+		if (!SetupDiGetDeviceInterfaceDetail(devs, &iface_data, NULL, 0, &detail_size, NULL)) {
+			const DWORD err = GetLastError();
+			if (err != ERROR_INSUFFICIENT_BUFFER) {
+				printError("SetupDiGetDeviceInterfaceDetail -- get size", err);
+				continue;
+			}
+		}
+
+		// Allocate the structure with the required size.
+		aAppDebugPrintf("      detail_size: %lu", detail_size);
+		SP_INTERFACE_DEVICE_DETAIL_DATA *detail = malloc(detail_size);
+		// Note how cbSize contains the static structure part, not the full payload.
+		detail->cbSize = sizeof(*detail);
+
+		// 2. Obtain the detail value fully. Also, obatin SP_DEVINFO_DATA, it is
+		// needed to get the registry key
+		SP_DEVINFO_DATA dev_data = {
+				.cbSize = sizeof(dev_data),
+		};
+		if (!SetupDiGetDeviceInterfaceDetail(devs, &iface_data, detail, detail_size, NULL, &dev_data)) {
+			printLastError("SetupDiGetDeviceInterfaceDetail -- get detail");
+			free(detail);
+			continue;
+		}
+		aAppDebugPrintf("      detail: %s", detail->DevicePath);
+
+		const BOOL found = (_stricmp(detail->DevicePath, monitor_id) == 0);
+
+		// Not needed anymore
+		free(detail);
+
+		if (!found)
+			continue;
+
+		aAppDebugPrintf("      DevicePath matches monitor");
+
+		const HKEY key = SetupDiOpenDevRegKey(devs, &dev_data, DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_READ);
+		if (key == INVALID_HANDLE_VALUE) {
+			printLastError("SetupDiOpenDevRegKey");
+			continue;
+		}
+
+		DWORD edid_size = sizeof(*out_edid);
+		DWORD reg_type;
+
+		const DWORD read_result = RegQueryValueEx(key, "EDID", NULL, &reg_type, out_edid->data, &edid_size);
+
+		// Not needed anymore
+		RegCloseKey(key);
+
+		if (read_result != ERROR_SUCCESS) {
+			printError("SetupDiOpenDevRegKey", read_result);
+			continue;
+		}
+
+		// TODO check edid_size
+		// TODO check reg_type
+
+		// Reached here = found edid, may exit
+		aAppDebugPrintf("      Read %lu EDID bytes", edid_size);
+		result = TRUE;
+		break;
+	}
+
+	SetupDiDestroyDeviceInfoList(devs);
+	return result;
+}
+
+static BOOL readEdidFromDisplayName(LPSTR device_name, EDID *out_edid) {
+	// EDD_GET_DEVICE_INTERFACE_NAME specifies that DISPLAY_DEVICE.DeviceID field
+	// will contain interface name that is congruent to GUID_DEVINTERFACE_MONITOR
+	// detail DevicePath. This is used in readEdidForDisplayPath().
+	DISPLAY_DEVICE monitor = {
+			.cb = sizeof(monitor),
+	};
+	if (0 == EnumDisplayDevices(device_name, 0, &monitor, EDD_GET_DEVICE_INTERFACE_NAME)) {
+		printLastError("EnumDisplayDevices");
+		return FALSE;
+	}
+
+	aAppDebugPrintf("  DISPLAY_DEVICE(%s):", device_name);
+	printDisplayDevice("    ", &monitor);
+
+	return readEdidForDisplayPath(monitor.DeviceID, out_edid);
+}
+
+typedef struct {
+	int x, y, w, h;
+	int raw_dpi;
+	EDID edid;
+} DisplayInfo;
+
+/*
+static void printDisplayInfo(const DisplayInfo *info) {
+	aAppDebugPrintf("#####################################################################");
+	aAppDebugPrintf("> FOUND DISPLAY %d,%d %dx%d dpi=%d", info->x, info->y, info->w, info->h, info->raw_dpi);
+	const int block_size = 16;
+	for (int i = 0; i < COUNTOF(info->edid.data); i += block_size) {
+		const BYTE *const b = info->edid.data + i;
+		fprintf(stderr, ">  ");
+		for (int j = 0; j < block_size; ++j)
+			fprintf(stderr, "%02x ", b[j]);
+		fprintf(stderr, "| ");
+		for (int j = 0; j < block_size; ++j)
+			fprintf(stderr, "%c", isprint(b[j]) ? b[j] : '.');
+		fprintf(stderr, "\n");
+	}
+	aAppDebugPrintf("#####################################################################");
+}
+*/
+
+static BOOL CALLBACK monitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
+	aAppDebugPrintf("monitorEnumProc(hMonitor=%p):", hMonitor);
+	(void)hdcMonitor;
+	(void)lprcMonitor;
+	(void)dwData;
+
+	MONITORINFOEX minfo = {
+			.cbSize = sizeof(minfo),
+	};
+	if (!GetMonitorInfo(hMonitor, (LPMONITORINFO)&minfo)) {
+		printLastError("GetMonitorInfo");
+		return TRUE;
+	}
+
+	aAppDebugPrintf("  szDevice: %s", minfo.szDevice);
+	aAppDebugPrintf(
+			"  rcMonitor: left=%ld top=%ld right=%ld bottom=%ld", minfo.rcMonitor.left, minfo.rcMonitor.top,
+			minfo.rcMonitor.right, minfo.rcMonitor.bottom
+	);
+	aAppDebugPrintf(
+			"  rcWork: left=%ld top=%ld right=%ld bottom=%ld", minfo.rcWork.left, minfo.rcWork.top, minfo.rcWork.right,
+			minfo.rcWork.bottom
+	);
+	aAppDebugPrintf("  dwFlags: %#lx%s", minfo.dwFlags, minfo.dwFlags & MONITORINFOF_PRIMARY ? ": PRIMARY" : "");
+
+	DEVMODE dev_mode = {
+			.dmSize = sizeof(dev_mode),
+	};
+	if (!EnumDisplaySettings(minfo.szDevice, ENUM_CURRENT_SETTINGS, &dev_mode)) {
+		printLastError("EnumDisplaySettings");
+		return TRUE;
+	}
+
+	aAppDebugPrintf("  DEVMODE:");
+	aAppDebugPrintf("    dmDeviceName: %s", dev_mode.dmDeviceName);
+	aAppDebugPrintf("    dmFields: %#lx", dev_mode.dmFields);
+	aAppDebugPrintf("    dmPelsWH: %lux%lu", dev_mode.dmPelsWidth, dev_mode.dmPelsHeight);
+	aAppDebugPrintf("    dmBitsPerPel: %lu", dev_mode.dmBitsPerPel);
+	aAppDebugPrintf("    dmDisplayFrequency: %lu", dev_mode.dmDisplayFrequency);
+
+	UINT raw_dpi_x, raw_dpi_y, eff_dpi_x, eff_dpi_y;
+	GetDpiForMonitor(hMonitor, MDT_RAW_DPI, &raw_dpi_x, &raw_dpi_y);
+	GetDpiForMonitor(hMonitor, MDT_EFFECTIVE_DPI, &eff_dpi_x, &eff_dpi_y);
+	aAppDebugPrintf("  DPI: effective=(%u, %u), raw=(%u, %u)", eff_dpi_x, eff_dpi_y, raw_dpi_y, raw_dpi_y);
+
+	DisplayInfo info = {
+			.x = minfo.rcMonitor.left,
+			.y = minfo.rcMonitor.top,
+			.w = dev_mode.dmPelsWidth,
+			.h = dev_mode.dmPelsHeight,
+			.raw_dpi = raw_dpi_x,
+	};
+	if (!readEdidFromDisplayName(minfo.szDevice, &info.edid)) {
+		aAppDebugPrintf("  Couldn't read EDID for this monitor, oh well");
+		return TRUE;
+	}
+
+	//printDisplayInfo(&info);
+
+	if (g.displays_count < A__MAX_DISPLAYS) {
+		AAppDisplay *const disp = &g.displays[g.displays_count++];
+		*disp = (AAppDisplay) {
+			.name = NULL,
+			.flags = AAPP_DISPLAY_HAS_EDID,
+			.width = info.w,
+			.height = info.h,
+			._.x = info.x,
+			._.y = info.y,
+		};
+		memcpy(disp->edid, info.edid.data, sizeof(disp->edid));
+	}
+
+	return TRUE;
+}
+
+static void enumerateDisplays(void) {
+	EnumDisplayMonitors(NULL, NULL, monitorEnumProc, 0);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASSEX wndclass;
 	ATimeUs last_paint = 0;
+	int x = 0, y = 0, w = ATTO_APP_WIDTH, h = ATTO_APP_HEIGHT, fullscreen = 0;
 
 	(void)hPrevInstance;
 	(void)lpCmdLine;
 
-	a__AppOpenConsole();
-
-	memset(&wndclass, 0, sizeof(wndclass));
-	wndclass.cbSize = sizeof(wndclass);
-	wndclass.lpszClassName = TEXT("atto");
-	wndclass.lpfnWndProc = a__AppWndProc;
-	wndclass.hInstance = hInstance;
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-	ATTO_ASSERT(RegisterClassEx(&wndclass));
-
-	g.hwnd = CreateWindow(TEXT("atto"), TEXT(ATTO_APP_NAME), WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS, 0,
-		0, ATTO_APP_WIDTH, ATTO_APP_HEIGHT, NULL, NULL, hInstance, NULL);
-	ATTO_ASSERT(0 != g.hwnd);
-	g.hdc = GetDC(g.hwnd);
-	ATTO_ASSERT(0 != g.hdc);
-
-	SetPixelFormat(g.hdc, ChoosePixelFormat(g.hdc, &a__pfd), &a__pfd);
-	g.hglrc = wglCreateContext(g.hdc);
-	ATTO_ASSERT(0 != g.hglrc);
-	wglMakeCurrent(g.hdc, g.hglrc);
+	//a__AppOpenConsole();
 
 	{
 		int i;
@@ -114,9 +345,62 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		a__app_state.argv = (const char **)argv;
 	}
 
+#ifdef ATTO_APP_PREINIT_FUNC
+	{
+		enumerateDisplays();
+		const AAppPreinitArgs args = {
+			.argc = a__app_state.argc,
+			.argv = (char *const *const)a__app_state.argv,
+			.displays = g.displays,
+			.displays_count = g.displays_count,
+		};
+		const AAppPreinitResult result = ATTO_APP_PREINIT_FUNC(&args);
+
+		if (result.fullscreen_display_index >= 0) {
+			const AAppDisplay *const disp = args.displays + result.fullscreen_display_index;
+			x = disp->_.x;
+			y = disp->_.y;
+			w = disp->width;
+			h = disp->height;
+			fullscreen = 1;
+			aAppDebugPrintf("Making a fullscreen window at display[%d] %s %d,%d %dx%d",
+				result.fullscreen_display_index, disp->name,
+				disp->_.x, disp->_.y, disp->width, disp->height);
+		}
+	}
+#endif
+	
+	{
+		const WNDCLASSEX wndclass = {
+			.cbSize = sizeof(wndclass),
+			.lpszClassName = TEXT("atto"),
+			.lpfnWndProc = a__AppWndProc,
+			.hInstance = hInstance,
+			.hCursor = LoadCursor(NULL, IDC_ARROW),
+			.hIcon = LoadIcon(NULL, IDI_APPLICATION),
+			.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
+		};
+		ATTO_ASSERT(RegisterClassEx(&wndclass));
+	}
+
+	g.hwnd = CreateWindow(TEXT("atto"), TEXT(ATTO_APP_NAME),
+		fullscreen
+		? WS_POPUP | WS_VISIBLE
+		: WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
+		x, y, w, h,
+		NULL, NULL, hInstance, NULL);
+	ATTO_ASSERT(0 != g.hwnd);
+	g.hdc = GetDC(g.hwnd);
+	ATTO_ASSERT(0 != g.hdc);
+
+	SetPixelFormat(g.hdc, ChoosePixelFormat(g.hdc, &a__pfd), &a__pfd);
+	g.hglrc = wglCreateContext(g.hdc);
+	ATTO_ASSERT(0 != g.hglrc);
+	wglMakeCurrent(g.hdc, g.hglrc);
+
 	a__app_state.gl_version = AOGLV_21;
-	a__app_state.width = ATTO_APP_WIDTH;
-	a__app_state.height = ATTO_APP_HEIGHT;
+	a__app_state.width = w;
+	a__app_state.height = h;
 
 	ATTO_APP_INIT_FUNC(&a__app_proctable);
 	if (a__app_proctable.resize)
